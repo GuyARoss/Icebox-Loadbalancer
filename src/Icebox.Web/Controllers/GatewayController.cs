f3a6a5c04ec90ea336f2c33315a9e0034ecb60ba
Guy Ross <guyallenross@gmail.com>
2019-05-29 16:53:28 -0400

    GatewayController - added a gateway method for accessing the icebox service.

---
+++ src/Icebox.Web/Controllers/GatewayController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Icebox.Core;
+using Icebox.Common;
+
+namespace Icebox.Web.Controllers
+{
+    [Route("gateway/{*any}")]
+    [ApiController]
+    public class GatewayController : ControllerBase
+    {        
+        public ActionResult<string> Invoke()
+        {
+            string pathValue = HttpContext.Request.Path.Value;
+
+            Tuple<string, GatewayMethodType> resolvedHanlder = IceboxServiceHandler.Invoke(pathValue);
+
+            if (resolvedHanlder.Item2 == GatewayMethodType.REDIRECT)
+            {
+                return new RedirectResult(resolvedHanlder.Item1);
+            } else
+            {
+                return resolvedHanlder.Item1;
+            }
+        }
+    }
+}
