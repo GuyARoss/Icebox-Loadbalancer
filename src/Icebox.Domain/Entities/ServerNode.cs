cc086b09e18c1a957dff70bbf12ed0a14835f749
Guy Ross <guyallenross@gmail.com>
2019-05-29 16:50:54 -0400

    ServerNode - added an additional property for a url address.

--- src/Icebox.Domain/Entities/ServerNode.cs
+++ src/Icebox.Domain/Entities/ServerNode.cs
@@ -5,5 +5,6 @@
         public uint Id { get; set; }
         public string Name { get; set; }   
         public string ClusterId { get; set; }
+        public string Address { get; set; }
     }
 }
