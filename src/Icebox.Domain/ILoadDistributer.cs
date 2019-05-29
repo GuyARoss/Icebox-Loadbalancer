02c4da6d53fb74c28c247ce9710d414f92b7dcc0
Guy Ross <guyallenross@gmail.com>
2019-05-29 16:52:29 -0400

    ILoadDistributor - changed the generic persistance type to be int instead of string.

--- src/Icebox.Domain/ILoadDistributer.cs
+++ src/Icebox.Domain/ILoadDistributer.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Isam.Esent.Collections.Generic;
 
 using Icebox.Common.Entities;
+using System.Threading.Tasks;
 
 namespace Icebox.Common
 {
     public interface ILoadDistributor
     {
-        string Invoke(IEnumerable<ServerNode> nodes, PersistentDictionary<string, string> store);
+        string Invoke(IEnumerable<ServerNode> nodes, PersistentDictionary<int, int> store);
     }
 }
