629860507761e45417f9da7f950a25cbf9d05c01
Guy Ross <guyallenross@gmail.com>
2019-05-29 16:49:22 -0400

    LoadBalancer - implemented IDisposable for the load balancer class. - fixed the select instance from pool name to be more meaningful.

--- src/Icebox.Core/LoadBalancing/LoadBalancer.cs
+++ src/Icebox.Core/LoadBalancing/LoadBalancer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Isam.Esent.Collections.Generic;
 
 using Icebox.Common;
@@ -6,24 +7,29 @@ using Icebox.Common.Entities;
 
 namespace Icebox.Core.LoadBalancing
 {
-    public class LoadBalancer
+    public class LoadBalancer : IDisposable
     {
         public readonly ILoadDistributor Distributor;
         public readonly IEnumerable<ServerNode> NodePool;
 
-        private readonly PersistentDictionary<string, string> _persistedDictionary;
+        private readonly PersistentDictionary<int, int> _persistedDictionary;
 
         public LoadBalancer(ILoadDistributor distributor, IEnumerable<ServerNode> nodePool, string cluserId)
         {
             Distributor = distributor;
             NodePool = nodePool;
 
-            _persistedDictionary = new PersistentDictionary<string, string>(cluserId);
+            _persistedDictionary = new PersistentDictionary<int, int>(cluserId);
         }
 
-        public string SelectFromPool()
-        {            
-            return Distributor.Invoke(NodePool, _persistedDictionary);
+        public void Dispose()
+        {
+            _persistedDictionary.Dispose();
+        }
+
+        public string SelectInstanceFromPool()
+        {        
+            return Distributor.Invoke(NodePool, _persistedDictionary);                        
         }        
     }
 }
