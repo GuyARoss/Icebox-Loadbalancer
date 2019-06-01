ce86fd0ef7c5ab660cf2025c1342135961d695dd
Guy Ross <guyallenross@gmail.com>
2019-06-01 19:58:07 -0400

    created a service registry service.

---
+++ src/Icebox.Service/ServiceRegistry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Icebox.Common.Entities;
+using Icebox.Persistance;
+
+namespace Icebox.Services
+{
+    public class ServiceRegistry
+    {
+        private static readonly ServiceRepository _repo = new ServiceRepository();
+        public static Task AddServerNode(ServiceModel model) => _repo.Create(model);
+        public static Task DeleteServerNode(ServiceModel model) => _repo.Delete(model);
+        public static ServiceModel FindById(string id) => _repo.FindById(id);
+        public static List<ServiceModel> FindAll() => _repo.FindAll().ToList();
+    }
+}
