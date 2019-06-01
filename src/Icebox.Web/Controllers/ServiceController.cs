ee6a10c3fbaa70a266149d460c088668fae89309
Guy Ross <guyallenross@gmail.com>
2019-06-01 19:59:21 -0400

    created a service controller.

---
+++ src/Icebox.Web/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Icebox.Common.Entities;
+using Icebox.Services;
+
+namespace Icebox.Web.Controllers
+{
+    [Route("service")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        [HttpPost("create")]
+        public ActionResult<Task> CreateCluster([FromBody] ServiceModel model) => ServiceRegistry.AddServerNode(model);
+
+        [HttpPost("destory")]
+        public ActionResult<Task> DeleteClusterNode([FromBody] ServiceModel model) => ServiceRegistry.DeleteServerNode(model);
+
+        [HttpGet("findById")]
+        public ActionResult<ServiceModel> FindById([FromQuery] string id) => ServiceRegistry.FindById(id);
+
+        [HttpGet("findAll")]
+        public ActionResult<List<ServiceModel>> FindAll() => ServiceRegistry.FindAll();
+
+    }
+}
