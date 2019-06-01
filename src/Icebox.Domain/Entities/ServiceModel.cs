bca40c358ea53afec99f7a2057c88de574f93fd5
Guy Ross <guyallenross@gmail.com>
2019-06-01 19:54:59 -0400

    created a service model

---
+++ src/Icebox.Domain/Entities/ServiceModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Icebox.Common.Entities
+{
+    public class ServiceModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+    }
+}
