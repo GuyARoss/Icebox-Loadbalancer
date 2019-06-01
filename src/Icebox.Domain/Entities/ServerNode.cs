00d4a381855d0f0f452ac676232baba5056c78b6
Guy Ross <guyallenross@gmail.com>
2019-06-01 19:54:41 -0400

    added service id support to the server node model.

--- src/Icebox.Domain/Entities/ServerNode.cs
+++ src/Icebox.Domain/Entities/ServerNode.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }   
         public string ClusterId { get; set; }
         public string Address { get; set; }
+        public string ServiceId { get; set; }
     }
 }
