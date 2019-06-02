ad183f9a669a5c1696b2b4afca5ec80197dcbb8c
Guy Ross <guyallenross@gmail.com>
2019-06-01 21:12:03 -0400

    Added cluster service model validation.

--- src/Icebox.Service/Clusters.cs
+++ src/Icebox.Service/Clusters.cs
@@ -12,8 +12,19 @@ namespace Icebox.Services
     public static class Clusters
     {
         private static readonly ClusterRepository _repo = new ClusterRepository();
+        private static readonly ServiceRepository _serviceRepo = new ServiceRepository();
 
-        public static Task AddCluster(ClusterModel model) => _repo.Create(model);
+        public static Task AddCluster(ClusterModel model)
+        {
+            var foundService = _serviceRepo.FindById(model.ServiceId);
+
+            if (foundService == null)
+            {
+                throw new Exception("Service Does Not Exist, To Create a Cluster a Valid Serice Should Exist");
+            }
+            
+            return _repo.Create(model);
+        }
         public static Task DeleteCluster(ClusterModel model) => _repo.Delete(model);
         public static ClusterModel FindById(string id) => _repo.FindById(id);
         public static List<ClusterModel> FindAll() => _repo.FindAll().ToList();
@@ -28,7 +39,6 @@ namespace Icebox.Services
 
             return typesDic;
         }
-
         public static Dictionary<string, int> GetGatewayTypes()
         {
             var typesDic = new Dictionary<string, int>();
