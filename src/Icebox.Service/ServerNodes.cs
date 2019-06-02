41207f687b9e71d2207eb2f634335ae9b7a59fde
Guy Ross <guyallenross@gmail.com>
2019-06-01 21:12:59 -0400

    added server node model validation.

--- src/Icebox.Service/ServerNodes.cs
+++ src/Icebox.Service/ServerNodes.cs
@@ -10,7 +10,25 @@ namespace Icebox.Services
     public static class ServerNodes
     {
         private static readonly ServerNodeRepository _repo = new ServerNodeRepository();
-        public static Task AddServerNode(ServerNode model) => _repo.Create(model);
+        private static readonly ClusterRepository _clusterRepository = new ClusterRepository();
+        private static readonly ServiceRepository _serviceRepo = new ServiceRepository();
+
+        public static Task AddServerNode(ServerNode model) // this process is too expensive.
+        {
+            var foundCluster = _clusterRepository.FindById(model.ClusterId);
+            int currentSize = _repo.FindByClusterId(model.ClusterId).Count();
+            var foundService = _serviceRepo.FindById(model.ServiceId);            
+
+            if (foundCluster != null && foundCluster.MaxSize > currentSize + 1)
+            {
+                throw new System.Exception(string.Format("Max Size for Cluster Exceeded, Current Limit is {0}", foundCluster.MaxSize));
+            } else if (foundService == null)
+            {
+                throw new System.Exception(string.Format("Service Does Not Exist, To Create a Cluster a Valid Serice Should Exist"));
+            }
+
+            return _repo.Create(model);
+        }
         public static Task DeleteServerNode(ServerNode model) => _repo.Delete(model);
         public static ServerNode FindById(string id) => _repo.FindById(id);
         public static List<ServerNode> FindAll() => _repo.FindAll().ToList();
