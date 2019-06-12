5bcc676688d31b32b0c4914e9dc3c244ecaf942b
Guy Ross <guyallenross@gmail.com>
2019-06-12 01:17:20 -0400

    added support for new persistence dictionary.

--- src/Icebox.Common/ILoadDistributer.cs
+++ src/Icebox.Common/ILoadDistributer.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Isam.Esent.Collections.Generic;
 
 using Icebox.Common.Entities;
 using System.Threading.Tasks;
+
+using Icebox.Infrastructure;
 
 namespace Icebox.Common
 {
     public interface ILoadDistributor
     {
-        string Invoke(IEnumerable<ServerNode> nodes, PersistentDictionary<int, int> store);
+        string Invoke(IEnumerable<ServerNode> nodes, PersistentDictionary<int> store);
     }
 }
